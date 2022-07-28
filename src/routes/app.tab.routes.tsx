58ed1800b57f99ff56dd05c5aba053ee7efcef65
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-07-27 21:50:20 -0300

    feat: create bottom tab indicator animation

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { Easing } from 'react-native-reanimated';
 
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useDynamicAnimation } from 'moti';
 import { useTheme } from 'styled-components';
 
 import { SearchScreen } from '~/screens/SearchScreen';
 
 import { StackRoutes } from './app.stack.routes';
+import { INDICATOR_WIDTH, MotiIndicator } from './styles';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function TabRoutes() {
   const theme = useTheme();
+
+  const animation = useDynamicAnimation();
+
+  const handleSelect = (index: number) => {
+    animation.animateTo({
+      translateX: index * INDICATOR_WIDTH,
+    });
+  };
 
   return (
     <Navigator
@@ -24,9 +35,19 @@ export function TabRoutes() {
         tabBarInactiveTintColor: theme.colors.textDetail,
         tabBarStyle: {
           paddingVertical: Platform.OS === 'ios' ? getBottomSpace() - 20 : 0,
-          height: 75,
+          height: getBottomSpace() + 60,
           backgroundColor: theme.colors.secondaryBackground,
         },
+        tabBarBackground: () => (
+          <MotiIndicator
+            state={animation}
+            transition={{
+              type: 'timing',
+              easing: Easing.inOut(Easing.ease),
+              duration: 400,
+            }}
+          />
+        ),
       }}
       initialRouteName="MyCitiesTabScreen"
     >
@@ -38,6 +59,9 @@ export function TabRoutes() {
             <Ionicons name="list-outline" size={24} color={color} />
           ),
         }}
+        listeners={() => ({
+          focus: () => handleSelect(0),
+        })}
       />
       <Screen
         name="SearchScreen"
@@ -47,6 +71,9 @@ export function TabRoutes() {
             <Ionicons name="search-outline" size={24} color={color} />
           ),
         }}
+        listeners={() => ({
+          focus: () => handleSelect(1),
+        })}
       />
     </Navigator>
   );
