849918b3f99e576e1a1b5ba69308fa4487d6d026
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-07-28 12:53:17 -0300

    feat: create city card add button animation

--- src/components/CityCard/index.tsx
+++ src/components/CityCard/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
 import { Ionicons } from '@expo/vector-icons';
+import { AnimatePresence, MotiView } from 'moti';
 import { useTheme } from 'styled-components';
 
 import { AddButton, CityInfo, CityName, Container, Country } from './styles';
+
+const presenceAnimation = {
+  from: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  transition: {
+    type: 'timing',
+    duration: 80,
+  },
+} as any;
 
 interface ICityCardData {
   name: string;
@@ -27,15 +47,21 @@ export function CityCard({ data }: ICityCard) {
       </CityInfo>
 
       <AddButton onPress={data.addCity} disabled={data.cityIsAlreadyStored}>
-        {data.cityIsAlreadyStored ? (
-          <Ionicons
-            name="checkmark-sharp"
-            size={24}
-            color={theme.colors.success}
-          />
-        ) : (
-          <Ionicons name="add-sharp" size={24} color={theme.colors.main} />
-        )}
+        <AnimatePresence exitBeforeEnter>
+          {data.cityIsAlreadyStored ? (
+            <MotiView key="check-icon" {...presenceAnimation}>
+              <Ionicons
+                name="checkmark-sharp"
+                size={24}
+                color={theme.colors.success}
+              />
+            </MotiView>
+          ) : (
+            <MotiView key="add-icon" {...presenceAnimation}>
+              <Ionicons name="add-sharp" size={24} color={theme.colors.main} />
+            </MotiView>
+          )}
+        </AnimatePresence>
       </AddButton>
     </Container>
   );
