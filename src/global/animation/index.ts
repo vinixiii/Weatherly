d3a1e5a8269a917a33e37d33d1e3cf8809f22bc6
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2023-10-02 14:18:06 -0300

    chore: create animation global file

---
+++ src/global/animation/index.ts
@@ -0,0 +1,18 @@
+export const presenceAnimation = {
+  from: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  transition: {
+    type: 'timing',
+    duration: 80,
+  },
+} as any;
